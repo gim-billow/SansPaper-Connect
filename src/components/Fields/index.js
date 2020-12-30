dcff84df2c4e45d5b56a97b53d5591ada1eddba9
kleav-billow <kleavant@billowsoftware.com.au>
2020-12-30 16:24:26 +0800

    added checkbox component

--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -4,6 +4,7 @@ import User from './User';
 import MultiSelect from './MultiSelect';
 import Select from './Select';
 import ToggleButton from './ToggleButton';
+import Checkbox from './Checkbox';
 
 export default {
   text: Text,
@@ -13,4 +14,5 @@ export default {
   user: User,
   select: Select,
   selectmulti: MultiSelect,
+  checkbox: Checkbox,
 };
