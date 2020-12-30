44f02297d766e2e43dc3c7b59989b270fb0d7dce
kleav-billow <kleavant@billowsoftware.com.au>
2020-12-30 16:00:05 +0800

    Add contact component

--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -4,6 +4,7 @@ import User from './User';
 import MultiSelect from './MultiSelect';
 import Select from './Select';
 import ToggleButton from './ToggleButton';
+import Contact from './Contact';
 
 export default {
   text: Text,
@@ -13,4 +14,5 @@ export default {
   user: User,
   select: Select,
   selectmulti: MultiSelect,
+  contact: Contact,
 };
