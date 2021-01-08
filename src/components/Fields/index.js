d358fc0fa50940aeb5db3ae8d5b456e4c5e49d60
kleav-billow <kleavant@billowsoftware.com.au>
2021-01-08 08:40:16 +0800

    added datetime picker component

--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -6,6 +6,7 @@ import Select from './Select';
 import ToggleButton from './ToggleButton';
 import DatePicker from './DatePicker';
 import TimePicker from './TimePicker';
+import DateTimePicker from './DateTimePicker';
 
 export default {
   text: Text,
@@ -17,4 +18,5 @@ export default {
   selectmulti: MultiSelect,
   date: DatePicker,
   time: TimePicker,
+  datetime: DateTimePicker,
 };
