064d54f162654ec43a406088454e2add490baa8b
kleav-billow <kleavant@billowsoftware.com.au>
2021-01-12 09:40:06 +0800

    added signaturePad component

---
+++ src/components/Fields/Signature/styles.js
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+import {spaceSmall, spaceMedium} from 'styles/space';
+import {regular} from 'styles/font';
+import {lightRed} from '@styles/colors';
+
+export default StyleSheet.create({
+  signatureColor: {
+    flex: 1,
+    backgroundColor: '#ffb3b3',
+    borderWidth: 1,
+    borderColor: lightRed,
+  },
+  label: {
+    fontSize: regular,
+    paddingTop: spaceSmall,
+    paddingLeft: spaceSmall,
+    paddingRight: spaceMedium,
+    paddingBottom: spaceSmall,
+  },
+  text: {
+    color: lightRed,
+    fontSize: 14,
+  },
+  container: {
+    width: 400,
+    height: 260,
+  },
+  signature: {
+    paddingLeft: spaceSmall,
+    paddingRight: spaceSmall,
+    width: '92%',
+    height: 200,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: spaceSmall,
+    paddingRight: spaceSmall,
+  },
+  button: {
+    paddingTop: spaceSmall,
+    paddingBottom: spaceSmall,
+    width: '46%',
+  },
+  buttonColor: {
+    backgroundColor: 'white',
+  },
+});
