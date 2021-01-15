b6150af486c8506329527fbf17c023dc61228b3b
kleav-billow <kleavant@billowsoftware.com.au>
2021-01-15 19:22:58 +0800

    enhance the date picker, add long-press to remove the date

--- src/components/Fields/DatePicker/index.js
+++ src/components/Fields/DatePicker/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {View, Text} from 'react-native';
-import {Button} from 'react-native-paper';
+import Toast from 'react-native-simple-toast';
+import {Button, TouchableRipple} from 'react-native-paper';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import styles from './styles';
 import ItemWrapper from '../ItemWrapper';
@@ -32,23 +33,35 @@ const DatePicker = (props) => {
     hideDatePicker();
   };
 
+  const cancel = () => {
+    if (label !== 'Select Date') {
+      Toast.show('Success, remove the date ' + label.toString());
+      setLabel('Select Date');
+      setChangeTheme(false);
+      updateFieldsValue({rank: item.rank, value: ''});
+    }
+  };
+
   return (
     <ItemWrapper>
       <Text style={styles.text}>{item.label}</Text>
       <View style={styles.date}>
-        <Button
-          mode="contained"
-          style={
-            changeTheme === true ? styles.ChangeButtonColor : styles.buttonColor
-          }
-          onPress={showDatePicker}>
-          <Text
+        <TouchableRipple onLongPress={cancel} onPress={showDatePicker}>
+          <Button
+            mode="contained"
             style={
-              changeTheme === true ? styles.ChangeTextColor : styles.TextColor
+              changeTheme === true
+                ? styles.ChangeButtonColor
+                : styles.buttonColor
             }>
-            {label.toString()}
-          </Text>
-        </Button>
+            <Text
+              style={
+                changeTheme === true ? styles.ChangeTextColor : styles.TextColor
+              }>
+              {label.toString()}
+            </Text>
+          </Button>
+        </TouchableRipple>
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
           mode="date"
