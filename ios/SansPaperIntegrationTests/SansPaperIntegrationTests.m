753e9b742e0ce13311dd43b62d72770f2b13bd64
kleav-billow <kleavant@billowsoftware.com.au>
2021-03-17 21:28:19 +0800

    quick fix on ios build

--- ios/SansPaperIntegrationTests/SansPaperIntegrationTests.m
+++ ios/SansPaperIntegrationTests/SansPaperIntegrationTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SansPaperConnectTests : XCTestCase
+@interface SansPaperIntegrationTests : XCTestCase
 
 @end
 
-@implementation SansPaperConnectTests
+@implementation SansPaperIntegrationTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
