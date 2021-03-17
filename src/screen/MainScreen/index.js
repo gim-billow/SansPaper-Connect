08a6e0e601bc16a42b0243354397db7a9713155b
kleav-billow <kleavant@billowsoftware.com.au>
2021-03-17 20:50:55 +0800

    Revert "Merge pull request #78 from Billow-Software/SPMNG-246"
    This reverts commit 2220380b52c6c46eb5e51cfec34d2d0212d6bece, reversing
    changes made to 05b409ffb8a7fc57abd98f17c7668d817e44e128.

--- src/screen/MainScreen/index.js
+++ src/screen/MainScreen/index.js
@@ -1,14 +1,13 @@
 //library
 import React from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {createStructuredSelector} from 'reselect';
 import HTML from 'react-native-render-html';
 import {map} from 'ramda';
 
 import styles from './styles';
-import {commonStyles} from '@styles/common';
-import {selectNews} from '@selector/common';
+import {selectNews} from '../../selector/common';
 
 class MainScreen extends React.Component {
   static options = () => {
@@ -24,18 +23,15 @@ class MainScreen extends React.Component {
   };
 
   render() {
-    const {updatedNews} = this.props;
-    const news = updatedNews.length ? updatedNews[0] : '';
+    const {htmlContent} = this.props;
 
     return (
       <View style={styles.container}>
-        {/<\/?[a-z][\s\S]*>/i.test(news.announcement) ? (
-          map(
-            (content) => <HTML key={content} html={content} />,
-            news.announcement,
-          )
-        ) : (
-          <Text style={commonStyles.text}>{news.announcement}</Text>
+        {map(
+          (content) => (
+            <HTML key={content} html={content} />
+          ),
+          htmlContent,
         )}
       </View>
     );
@@ -43,7 +39,7 @@ class MainScreen extends React.Component {
 }
 
 const mapState = createStructuredSelector({
-  updatedNews: selectNews,
+  htmlContent: selectNews,
 });
 
 export default connect(mapState, {})(MainScreen);
