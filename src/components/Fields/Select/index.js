bc2dfdef5ca4db32246181f9d59eea7817c33a0a
kleav-billow <kleavant@billowsoftware.com.au>
2021-04-01 16:31:19 +0800

    fix bug tickets

--- src/components/Fields/Select/index.js
+++ src/components/Fields/Select/index.js
@@ -1,31 +1,27 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import {Text, View} from 'react-native';
 import {Divider} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
+import R from 'ramda';
 
 import ItemWrapper from '../ItemWrapper';
 import styles from './styles';
 import MandatoryField from '../MandatoryField';
 import {getQueryByOptions} from './helper';
 import {selectProjectValue} from 'selector/form';
-import R from 'ramda';
 import {commonStyles} from '@styles/common';
 
-class Select extends Component {
+class Select extends PureComponent {
   state = {
     options: [],
-    selOptions: [
-      {
-        id: '',
-        name: 'None',
-      },
-    ],
+    selOptions: [],
   };
 
   async componentDidMount() {
+    let selected = [];
     const {seloptions, type} = this.props.item;
     const {organization, projectValue, item} = this.props;
     const options = await getQueryByOptions(
@@ -35,7 +31,13 @@ class Select extends Component {
       projectValue,
     );
 
-    this.updateSetOptions(options, [item.value]);
+    if (item.type === 'selectmulti' && item.value) {
+      selected = item.value.split('|');
+    } else {
+      selected = [item.value];
+    }
+
+    this.updateSetOptions(options, selected);
   }
 
   async componentDidUpdate(prevProps) {
@@ -55,70 +57,57 @@ class Select extends Component {
         this.updateSetOptions(options, [item.value]);
         return;
       }
-
-      if (item.type !== 'selectmulti') {
-        const options = await getQueryByOptions(this.props);
-        this.updateSetOptions(options, [item.value]);
-      }
     }
   }
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if (this.props.item.seloptions.includes('projects.milestones')) {
-  //     if (this.props.projectValue !== nextProps.projectValue) {
-  //       return true;
-  //     }
-  //     return false;
-  //   }
-
-  //   if (this.props.item.value !== nextProps.item.value) {
-  //     return true;
-  //   } else if (this.state.selOptions.length === 1) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
   resetSelOptions = () => {
     this.setState({
-      selOptions: [
-        {
-          id: '',
-          name: 'None',
-        },
-      ],
+      selOptions: [],
     });
   };
 
   updateSetOptions = (options, value) => {
-    const {item} = this.props;
     const filteredOptions = R.pipe(
       R.sortBy(R.compose(R.toLower, R.prop('name'))),
       R.filter((option) => !R.isNil(option)),
     )(options);
-    this.setState((prevState) => ({
-      selOptions:
-        item.type === 'selectmulti'
-          ? [...filteredOptions]
-          : [...prevState.selOptions, ...filteredOptions],
-      options: value[0] ? value : [], // if 1st value is empty string, set to empty string
-    }));
+
+    this.setState({selOptions: [...filteredOptions], options: value});
   };
 
   onSelectedItemsChange = (selectedItems) => {
     const {item, updateFieldsValue} = this.props;
     let value = '';
 
-    selectedItems.map((items) => {
-      value += items.toString() + '|';
+    // if single, remove the selected option
+    if (
+      item.type !== 'selectmulti' &&
+      selectedItems[0] === this.state.options[0]
+    ) {
+      this.setState({options: []});
+      updateFieldsValue({
+        rank: item.rank,
+        value: '',
+      });
+
+      return;
+    }
+
+    selectedItems.map((selected, index) => {
+      let addedItem = null;
+      if (index === 0) {
+        addedItem = selected;
+      } else {
+        addedItem = `|${selected}`;
+      }
+      value += addedItem;
     });
 
-    this.setState({options: [...selectedItems]});
+    this.setState({options: R.filter((x) => x !== '', selectedItems)});
 
     updateFieldsValue({
       rank: item.rank,
-      value: item.type === 'selectmulti' ? value : selectedItems[0] + '',
+      value: item.type === 'selectmulti' ? value : selectedItems[0],
     });
   };
 
@@ -145,9 +134,7 @@ class Select extends Component {
                 itemText,
                 chipsWrapper,
               }}
-              items={selOptions.filter((selItem) => {
-                return selItem.name !== '';
-              })}
+              items={selOptions}
               searchPlaceholderText="Search from items"
               IconRenderer={Icon}
               uniqueKey="id"
